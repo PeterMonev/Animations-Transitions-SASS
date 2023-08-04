ba708323ef6c23a08e687313fd822973d033ab10
PeterMonev <monevpeter@gmail.com>
2023-08-04 19:26:33 +0300

    Creating ripple effect ot buttons

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ $(document).ready(function () {
 
     $("#showRegisterForm").click(function (event) {
         event.preventDefault();
-        $("#loginContainer").animate({ left: '-100%' }, 500).fadeOut(300, function() {
+        $("#loginContainer").animate({ left: '0%' }, 700).fadeOut(500, function() {
             $(this).hide();
         });
-        $("#registerContainer").fadeIn(300).show().animate({ left: '-100%' }, 500);
+        $("#registerContainer").fadeIn(1500).show().animate({ left: '0%' }, 1000);
     });
 
 // Animation register move in logic
     
     $("#showLoginForm").click(function (event) {
         event.preventDefault();
-        $("#registerContainer").animate({ left: '100%' }, 500).fadeOut(300, function() {
+        $("#registerContainer").animate({ left: '75%' }, 700).fadeOut(500, function() {
             $(this).hide();
         });
-        $("#loginContainer").css('left', '-100%').fadeIn(300).show().animate({ left: '40%' }, 500);
+        $("#loginContainer").css('left', '0%').fadeIn(1500).show().animate({ left: '40%' }, 1000);
     });
 
 // Placeholder hide logic
@@ -27,5 +27,36 @@ $(document).ready(function () {
     }).blur(function () {
         $(this).attr("placeholder", $(this).attr("data-placeholder"));
     });
+
+// Button ripple function 
+
+function RippleStyle(width, height, posX, posY){
+    this.width = (width <= height) ? height : width;
+    this.height = (width <= height) ? height : width;
+    this.top = posY - (this.height * .5);
+    this.left = posX - (this.width * .5);
+}
+
+$('.btn').click(function(e) {
+    const rippleEl = $('<span class="btn-ripple"></span>').appendTo(this);
+   
+    const pos = $(this).offset();
+
+    const width = $(this).outerWidth();
+    const height = $(this).outerHeight();
+
+    const posX = e.pageX - pos.left;
+    const posY = e.pageY - pos.top;
+
+    const rippleStyle = new RippleStyle(width, height, posX, posY);
+
+    rippleEl.css(rippleStyle);
+   
+});
+
+$('.btn').on('animationend webkitAnimationEnd oanimationend MSAanimationEnd', '.btn-ripple', function(){
+  $(this).remove();
+})
+
   });
   
