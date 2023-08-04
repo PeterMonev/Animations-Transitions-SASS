a23a10a9a17aff76622208497a587d59f56c2aa2
PeterMonev <monevpeter@gmail.com>
2023-08-04 17:08:17 +0300

    Creating placeholder hide when click

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+// Animation login move out logic
+
     $("#showRegisterForm").click(function (event) {
         event.preventDefault();
         $("#loginContainer").animate({ left: '-100%' }, 500).fadeOut(300, function() {
@@ -6,7 +9,9 @@ $(document).ready(function () {
         });
         $("#registerContainer").fadeIn(300).show().animate({ left: '-100%' }, 500);
     });
-  
+
+// Animation register move in logic
+    
     $("#showLoginForm").click(function (event) {
         event.preventDefault();
         $("#registerContainer").animate({ left: '100%' }, 500).fadeOut(300, function() {
@@ -14,7 +19,13 @@ $(document).ready(function () {
         });
         $("#loginContainer").css('left', '-100%').fadeIn(300).show().animate({ left: '40%' }, 500);
     });
-  
 
+// Placeholder hide logic
+  
+    $("input").focus(function () {
+        $(this).attr("data-placeholder", $(this).attr("placeholder")).attr("placeholder", "");
+    }).blur(function () {
+        $(this).attr("placeholder", $(this).attr("data-placeholder"));
+    });
   });
   
